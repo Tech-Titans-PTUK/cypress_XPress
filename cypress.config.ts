24123b52c9c80f1a9114edbd04b929978a71b70e
Mohammad Abohasan <mo7ammad.saed@gmail.com>
2023-12-08 13:29:25 +0200

    Modify viewport height and width in e2e configuration

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:5173",
+    viewportHeight: 900,
+    viewportWidth: 1500,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
